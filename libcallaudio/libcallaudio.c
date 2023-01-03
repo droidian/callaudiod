droidian/callaudiod

commit 932baf3112ba836454c42e793521fdd7c8c1d676

libcallaudio: Distinguish DBus error from unsuccessful operations
This gives users a better idea of what went wrong and avoids derefencing
error if it's NULL.

Operations can be unsuccessful (e.g. on a desktop machine where the
audio card does not expose earpiece/speaker ports) and lead to a
segmentation fault.

Before 4358323e4452da1c87f7c74a9dd759c34c9a671a an error was always
returned in the unsuccessful case, so that the GError was never NULL.

See the following gdb session

Thread 1 "gnome-calls" received signal SIGSEGV, Segmentation fault.
0x00007ffff70644ee in select_mode_done (object=<optimized out>, result=0x55555669e9a0, data=0x5555566b6800) at ../libcallaudio/libcallaudio.c:111
111     ../libcallaudio/libcallaudio.c: No such file or directory.
(gdb) bt
    at ../libcallaudio/libcallaudio.c:111
[snip]
(gdb) p error
$1 = (GError *) 0x0
(gdb) p success
$2 = 0
(gdb) p ret
$3 = 1

libcallaudio/libcallaudio.c
@@ -107,9 +107,10 @@ static void select_mode_done(GObject *object, GAsyncResult *result, gpointer dat
 
     ret = call_audio_dbus_call_audio_call_select_mode_finish(proxy, &success,
                                                              result, &error);
-    if (!ret || !success) {
-        g_warning("SelectMode failed with code %d: %s", success, error->message);
-    }
+    if (!ret)
+        g_warning("SelectMode DBus method invocation failed: %s", error->message);
+    else if (success)
+        g_warning("SelectMode unsuccessful");
 
     g_debug("%s: D-bus call returned %d (success=%d)", __func__, ret, success);
 
@@ -198,9 +199,10 @@ static void enable_speaker_done(GObject *object, GAsyncResult *result, gpointer 
 
     ret = call_audio_dbus_call_audio_call_enable_speaker_finish(proxy, &success,
                                                                 result, &error);
-    if (!ret || !success) {
-        g_warning("EnableSpeaker failed with code %d: %s", success, error->message);
-    }
+    if (!ret)
+        g_warning("EnableSpeaker DBus method invocation failed: %s", error->message);
+    else if (success)
+        g_warning("EnableSpeaker unsuccessful");
 
     g_debug("%s: D-bus call returned %d (success=%d)", __func__, ret, success);
 
@@ -290,8 +292,10 @@ static void mute_mic_done(GObject *object, GAsyncResult *result, gpointer data)
 
     ret = call_audio_dbus_call_audio_call_mute_mic_finish(proxy, &success,
                                                           result, &error);
-    if (!ret || !success)
-        g_warning("MuteMic failed with code %d: %s", success, error->message);
+    if (!ret)
+        g_warning("MuteMic DBus method invocation failed: %s", error->message);
+    else if (success)
+        g_warning("MuteMic unsuccessful");
 
     g_debug("%s: D-bus call returned %d (success=%d)", __func__, ret, success);
 

commit 0755fde62f2a7b439eca7ff1a51cdc46ab69d89a

libcallaudio: Properly handle successful return code
Despite successful operation libcallaudio complains about return code.
```
mobian@mobian-dev:~ $ G_MESSAGES_DEBUG=all callaudiocli -m 0
(callaudiocli:25219): libcallaudio-WARNING **: 01:39:39.174: SelectMode (0) unsuccessful
(callaudiocli:25219): libcallaudio-DEBUG: 01:39:39.175: SelectMode succeeded: success=1
```

This behaviour is regression introduced by commit
932baf3112ba836454c42e793521fdd7c8c1d676 (libcallaudio: Distinguish DBus error from unsuccessful operations).

libcallaudio/libcallaudio.c
@@ -109,7 +109,7 @@ static void select_mode_done(GObject *object, GAsyncResult *result, gpointer dat
                                                              result, &error);
     if (!ret)
         g_warning("SelectMode DBus method invocation failed: %s", error->message);
-    else if (success)
+    else if (!success)
         g_warning("SelectMode unsuccessful");
 
     g_debug("%s: D-bus call returned %d (success=%d)", __func__, ret, success);
@@ -168,7 +168,7 @@ gboolean call_audio_select_mode(CallAudioMode mode, GError **error)
                                                            NULL, error);
     if (!ret) {
         g_warning("SelectMode DBus method invocation failed: %s", (*error)->message);
-    } else if (success) {
+    } else if (!success) {
         g_warning("SelectMode (%u) unsuccessful", mode);
         g_set_error_literal(error, G_IO_ERROR, G_IO_ERROR_FAILED, "Operation failed");
     }
@@ -205,7 +205,7 @@ static void enable_speaker_done(GObject *object, GAsyncResult *result, gpointer 
                                                                 result, &error);
     if (!ret)
         g_warning("EnableSpeaker DBus method invocation failed: %s", error->message);
-    else if (success)
+    else if (!success)
         g_warning("EnableSpeaker unsuccessful");
 
     g_debug("%s: D-bus call returned %d (success=%d)", __func__, ret, success);
@@ -278,7 +278,7 @@ gboolean call_audio_enable_speaker(gboolean enable, GError **error)
                                                               NULL, error);
     if (!ret) {
         g_warning("EnableSpeaker DBus method invocation failed: %s", (*error)->message);
-    } else if (success) {
+    } else if (!success) {
         g_warning("EnableSpeaker (%sable) unsuccessful", enable ? "en" : "dis");
         g_set_error_literal(error, G_IO_ERROR, G_IO_ERROR_FAILED, "Operation failed");
     }
@@ -302,7 +302,7 @@ static void mute_mic_done(GObject *object, GAsyncResult *result, gpointer data)
                                                           result, &error);
     if (!ret)
         g_warning("MuteMic DBus method invocation failed: %s", error->message);
-    else if (success)
+    else if (!success)
         g_warning("MuteMic unsuccessful");
 
     g_debug("%s: D-bus call returned %d (success=%d)", __func__, ret, success);
@@ -361,7 +361,7 @@ gboolean call_audio_mute_mic(gboolean mute, GError **error)
                                                         NULL, error);
     if (!ret) {
         g_warning("MuteMic DBus method invocation failed: %s", (*error)->message);
-    } else if (success) {
+    } else if (!success) {
         g_warning("MuteMic (%smute) unsuccessful", mute ? "" : "un");
         g_set_error_literal(error, G_IO_ERROR, G_IO_ERROR_FAILED, "Operation failed");
     }